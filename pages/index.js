e79a80e8739af91d4a8070df1a8bccced68a6dab
geissoncr <geissoncr@gmail.com>
2021-07-15 20:41:08 -0300

    primeiro comit

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+
+function ProfileSideBar(propriedades) {
+
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.giyhubUser}.png`} style={{ borderRadius: '8px' }} />
+      {/* Imagem */}
+    </Box>
+  )
+}
+
+export default function Home() {
+  const giyhubUser = 'geissoncr'
+  const pessoasFavoritas = ['andreclaudino', 'Brutos212', 'peas', 'VitorSavedra', 'felipefialho']
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSideBar giyhubUser={giyhubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className='title'>
+              Bem-vindo (a)
+            </h1>
+
+            <OrkutNostalgicIconSet/>
+          </Box>
+        </div>
+        <div className="profileRelationArea" style={{ gridArea: 'profileRelationArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade({pessoasFavoritas.length}):
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+
+            </ul>
+
+
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
+}
